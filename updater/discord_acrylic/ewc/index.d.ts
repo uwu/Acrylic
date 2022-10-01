d2b60cace3f1705d1d0a909cd2478a55b20dce3e
alethéia <me@xirreal.dev>
2022-10-01 21:22:19 +0200

    v2

--- updater/discord_acrylic/ewc/index.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="electron"/>
-export function disable(window: Electron.BrowserWindow, color: number): boolean;
-export function setGradient(window: Electron.BrowserWindow, color: number): boolean;
-export function setTransparentGradient(window: Electron.BrowserWindow, color: number): boolean;
-export function setBlurBehind(window: Electron.BrowserWindow, color: number): boolean;
-export function setAcrylic(window: Electron.BrowserWindow, color: number): boolean;
